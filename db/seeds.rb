254179e39c73afc89f2d3401687fb52f54d8ec86
Scott Murray <rai.five@gmail.com>
2017-10-06 14:58:11 +0100

    App seeded - CRUD operations all working

---
+++ db/seeds.rb
@@ -0,0 +1,62 @@
+require_relative("../models/films")
+require_relative("../models/customers")
+require_relative("../models/tickets")
+
+require( 'pry-byebug' )
+
+Customer.delete_all()
+Film.delete_all()
+
+customer1 = Customer.new({'name' => 'Scott Murray', 'funds' => 100})
+customer1.save()
+
+film1 = Film.new({'name' => 'Blade Runner 2049', 'price' => 10})
+film1.save()
+
+
+binding.pry
+
+nil
+
+
+
+
+
+
+
+
+
+
+
+# require_relative( '../models/user' )
+# require_relative( '../models/location' )
+# require_relative( '../models/visit' )
+#
+# require( 'pry-byebug' )
+#
+# Visit.delete_all()
+# Location.delete_all()
+# User.delete_all()
+#
+# user1 = User.new({ 'name' => 'Samwise Gamgee' })
+# user1.save()
+# user2 = User.new({ 'name' => 'Gollum' })
+# user2.save()
+#
+# location1 = Location.new({ 'category' => 'Attractions', 'name' => 'Mordor'})
+# location1.save()
+# location2 = Location.new({ 'category' => 'Places To Go', 'name' => 'Hobbiton'})
+# location2.save()
+#
+# visit1 = Visit.new({ 'user_id' => user1.id, 'location_id' => location1.id, 'review' => '0 stars, far too hot'})
+# visit1.save()
+#
+# visit2 = Visit.new({ 'user_id' => user2.id, 'location_id' => location1.id, 'review' => '5 stars, would visit again if I could'})
+# visit2.save()
+#
+# visit3 = Visit.new({ 'user_id' => user1.id, 'location_id' =>
+# location2.id, 'review' => '4 stars, plenty of beer available'})
+# visit3.save()
+#
+# binding.pry
+# nil
